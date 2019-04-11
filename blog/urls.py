a2360eeccf66c151eafc61f8daff922ccf44a69d
Andrew Rietz <andrew.rietz@gmail.com>
2019-04-11 00:05:23 -0600

    Site is fully functional. Resume added, blogs accessible, navbar hooked up

--- blog/urls.py
+++ blog/urls.py
@@ -18,4 +18,5 @@ from . import views
 
 urlpatterns = [
     path('', views.allblogs, name = 'allblogs'),
+    path('<int:blog_id>/', views.detail, name='detailed_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -6,3 +6,7 @@ from .models import Blog
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    detailed_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detailed_blog.html', {'blog':detailed_blog})
