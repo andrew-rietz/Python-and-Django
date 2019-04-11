13e74667ce4aa786d843c362c53827638b438900
Andrew Rietz <andrew.rietz@gmail.com>
2019-04-10 23:06:22 -0600

    Added blogs and changed object display in site/admin

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-"""portfolio URL Configuration
+"""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-import jobs.views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', jobs.views.home, name = 'Home'),
-    path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('', views.allblogs, name = 'allblogs'),
+]
