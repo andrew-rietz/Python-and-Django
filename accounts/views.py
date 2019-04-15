9ec631bfb62287811379886771e05001c26f9e03
Andrew Rietz <andrew.rietz@gmail.com>
2019-04-14 22:08:29 -0600

    Started setting html for each page. Set up accounts framework + validation. Next, on to sign in/out.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        #User has info and wants an account now
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/Sign Up.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            user = User.objects.get(username=request.POST['username'])
+            return render(request, 'accounts/Sign Up.html', {'error':'Passwords must match'})
+    else:
+        #User wants to enter info
+        return render(request, 'accounts/Sign Up.html')
+
+def login(request):
+    return render(request, 'accounts/Login.html')
+
+def logout(request):
+    # TODO Need to route to homepage
+    # and don't forget to logout
+    return render(request, 'accounts/Sign Up.html')
