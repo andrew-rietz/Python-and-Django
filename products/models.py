ee9793f241769b3db17ccb22122bff3c695eca89
Andrew Rietz <andrew.rietz@gmail.com>
2019-04-15 22:34:32 -0600

    Added products, product details, user auth, and finalized home page. Site is functional, but bare bones.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+
+    '''
+    The hunter is the ID number of a given user
+    The ID is called the primary key (when referencing self)
+    Otherwise, known as foreign key
+
+    The 'on_delete' says that if the user is deleted, then
+    the products associated with the user are deleted as well
+    '''
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
