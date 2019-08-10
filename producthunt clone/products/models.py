cd63e6387451a6b62f87c002e2cc645e410b9a95
Andrew Rietz <andrew.rietz@gmail.com>
2019-08-09 23:51:09 -0600

    Added settings files to gitignore. Updated models in producthunt

--- producthunt clone/products/models.py
+++ producthunt clone/products/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Product(models.Model):
 
     title = models.CharField(max_length=255)
     url = models.TextField()
     pub_date = models.DateTimeField()
     votes_total = models.IntegerField(default=1)
-    image = models.ImageField(upload_to='images/')
-    icon = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")
     body = models.TextField()
 
-    '''
+    """
     The hunter is the ID number of a given user
     The ID is called the primary key (when referencing self)
     Otherwise, known as foreign key
 
     The 'on_delete' says that if the user is deleted, then
     the products associated with the user are deleted as well
-    '''
+    """
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -28,4 +29,4 @@ class Product(models.Model):
         return self.body[:100]
 
     def pub_date_pretty(self):
-        return self.pub_date.strftime('%b %e, %Y')
+        return self.pub_date.strftime("%b %e, %Y")
